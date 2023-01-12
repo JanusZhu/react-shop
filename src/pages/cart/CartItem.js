ec499b3828b0c93a2ca194adabb230bce6bb281e
JanusZhu <janus.zhu@yale.edu>
2023-01-12 00:21:37 -0500

    Updated Cart style

--- src/pages/cart/CartItem.js
+++ src/pages/cart/CartItem.js
@@ -8,7 +8,7 @@ export const CartItem = (props) => {
   return (
     <div className="border-solid rounded border-2 border-dark mx-20 p-8 shadow-md  shadow-black grid grid-cols-2 justify-center justify-items-center items-center">
       <div className="justify-self-end">
-        <img className="w-52 h-60" src={image} alt={`product${id}`} />
+        <img className="w-40 h-48" src={image} alt={`product${id}`} />
       </div>
       <div className="flex flex-col justify-center items-center gap-2">
         <p className="text-1xl font-bold">{title}</p>
