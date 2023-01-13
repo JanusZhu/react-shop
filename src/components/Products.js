70b081e9079619c229ca32a1c884a99c187d26fd
JanusZhu <janus.zhu@yale.edu>
2023-01-12 20:19:22 -0500

    Set up Firebase and added loading

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,41 @@
 import React from "react";
 import { Product } from "./Product";
-import { PRODUCTS } from "../assets/productInfo/products";
-
+import { useState, useEffect } from "react";
+import loadingGif from "../assets/loading.gif";
 export const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        "https://react-http-6bf46-default-rtdb.firebaseio.com/products.json      "
+      );
+      const responseData = await response.json();
+      const productsList = [];
+      for (const key in responseData) {
+        productsList.push({
+          id: responseData[key].id,
+          price: responseData[key].price,
+          title: responseData[key].title,
+          image: responseData[key].image,
+        });
+      }
+      //console.log(productsList);
+      setProducts(productsList);
+      setIsLoading(false);
+    };
+    fetchProducts();
+  }, []);
   return (
     <>
+      {isLoading && (
+        <div className="flex flex-col justify-center items-center mt-32">
+          <p>Loading</p>
+          <img className="w-12 h-12" src={loadingGif} alt="loadingGif" />
+        </div>
+      )}
       <div className=" grid grid-cols-3">
-        {PRODUCTS.map((product) => {
+        {products.map((product) => {
           return <Product key={product.id} product={product} />;
         })}
       </div>
--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,15 +1,38 @@
-import React, { useContext } from "react";
-import { PRODUCTS } from "../../assets/productInfo/products";
+import React, { useContext, useState, useEffect } from "react";
 import { ShopContext } from "../../context/shop-context";
 import { CartItem } from "./CartItem";
 import { useNavigate } from "react-router-dom";
 import emptyCart from "../../assets/emptyCart.png";
+import loadingGif from "../../assets/loading.gif";
 
 export const Cart = () => {
   const { cartItems, calSubtotal, clearCart, calNum } = useContext(ShopContext);
   const subtotal = calSubtotal();
   const itemNum = calNum();
   const navigate = useNavigate();
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        "https://react-http-6bf46-default-rtdb.firebaseio.com/products.json      "
+      );
+      const responseData = await response.json();
+      const productsList = [];
+      for (const key in responseData) {
+        productsList.push({
+          id: responseData[key].id,
+          price: responseData[key].price,
+          title: responseData[key].title,
+          image: responseData[key].image,
+        });
+      }
+      //console.log(productsList);
+      setProducts(productsList);
+      setIsLoading(false);
+    };
+    fetchProducts();
+  }, []);
   return (
     <div className="flex flex-col">
       <div className="flex justify-start items-end py-8 pl-8">
@@ -18,14 +41,20 @@ export const Cart = () => {
       </div>
 
       {subtotal === 0 || subtotal === "0.00" ? (
-        <div className="flex flex-col justify-center items-center gap-12">
+        <div className="flex flex-col justify-center items-center gap-12 mb-20">
           <img className="w-40 h-30" src={emptyCart} alt="emptyCart" />
           <h1>Cart is empty. Add some products to the cart.</h1>
         </div>
       ) : (
-        <div className="flex pl-8 gap-8 justify-start items-start mb-20 pb-0">
+        <div className="flex pl-8 gap-8 justify-start items-start mb-20">
           <div className="flex flex-col gap-8 w-3/5 border-solid rounded border-2 border-dark m-0 justify-bewteen shadow-md shadow-black pt-8">
-            {PRODUCTS.map((product) => {
+            {isLoading && (
+              <div className="flex flex-col justify-center items-center mt-32">
+                <p>Loading</p>
+                <img className="w-12 h-12" src={loadingGif} alt="loadingGif" />
+              </div>
+            )}
+            {products.map((product) => {
               if (cartItems[product.id] !== 0) {
                 return <CartItem key={product.id} data={product} />;
               } else {
